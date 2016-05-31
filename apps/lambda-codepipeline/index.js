75c2fc4106f4d4f8a3ced5baacdbcaeb7d536e62
Saginaw, Jonathan M <jonathan.m.saginaw@accenture.com>
2016-05-31 18:52:23 -0400

    updated lambda-codepipeline index file to include boilerplate code

--- apps/lambda-codepipeline/index.js
+++ apps/lambda-codepipeline/index.js
@@ -1,5 +1,9 @@
 //TODO: replace boiler-plate SNS code with code that puts message into Dynamo
 
+//TODO required when run in AWS?
+var AWS = require("aws-sdk");
+AWS.config.update({region: "us-east-1"});
+
 'use strict';
 console.log('Loading function');
 
@@ -10,4 +14,30 @@ exports.handler = (event, context, callback) => {
     //const message = event.Records[0].Sns.Message;
     //console.log('From SNS:', message);
     //callback(null, message);
+
+
+    //TODO use the dynamohelper once its extracted into its own module
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var tableName = 'testResultsTable';
+    var id = '1010' //to replace
+
+    //these will be pulled from the EVENT via CodePipeline
+    var params = {
+        TableName : 'testResultsTable',
+        Item: {
+            id: '1050',
+            date: 'May31',
+            harness: 'regression',
+            passpercentage:  '80'
+        }
+    };
+
+    docClient.put(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log(data);
+    });
+
+
 };
