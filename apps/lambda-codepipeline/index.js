47dabcc89461f77cec3d2da6a30ccdd8f08c569d
Saginaw, Jonathan M <jonathan.m.saginaw@accenture.com>
2016-05-31 21:17:57 -0400

    code pipeline lambda works based on AWS sample.  Next: replace w/Dynamo logic

--- apps/lambda-codepipeline/index.js
+++ apps/lambda-codepipeline/index.js
@@ -1,21 +1,73 @@
 //TODO: replace boiler-plate SNS code with code that puts message into Dynamo
 
-//TODO required when run in AWS?
 var AWS = require("aws-sdk");
 AWS.config.update({region: "us-east-1"});
 
 'use strict';
 console.log('Loading function');
 
-exports.handler = (event, context, callback) => {
-    console.log('EVENT:  \n', JSON.stringify(event, null, 2));
-    console.log('passpercentage:  ', event.passpercentage)
-    console.log('CONTEXT:  \n', context);
-    //const message = event.Records[0].Sns.Message;
-    //console.log('From SNS:', message);
-    //callback(null, message);
+exports.handler = function(event, context) {
+
+    var codepipeline = new AWS.CodePipeline();
+
+    // Retrieve the Job ID from the Lambda action
+    var jobId = event["CodePipeline.job"].id;
+
+    //use this as a user param
+    //var userParameters = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters;
+
+    // Notify AWS CodePipeline of a successful job
+    var putJobSuccess = function(message) {
+        var params = {
+            jobId: jobId
+        };
+        codepipeline.putJobSuccessResult(params, function(err, data) {
+            if(err) {
+                context.fail(err);
+            } else {
+                context.succeed(message);
+            }
+        });
+    };
+
+    // Notify AWS CodePipeline of a failed job
+    var putJobFailure = function(message) {
+        var params = {
+            jobId: jobId,
+            failureDetails: {
+                message: JSON.stringify(message),
+                type: 'JobFailed',
+                externalExecutionId: context.invokeid
+            }
+        };
+        codepipeline.putJobFailureResult(params, function(err, data) {
+            context.fail(message);
+        });
+    };
 
 
+    // Helper function to make a HTTP GET request to the page.
+    // The helper will test the response and succeed or fail the job accordingly
+    var getPage = function(text, callback) {
+        var pageObject = "hello";
+        callback(pageObject)
+    };
+
+    var url = "mytestdata";
+
+    getPage(url, function(returnedPage) {
+        try {
+            // Succeed the job
+            putJobSuccess("Tests passed.");
+        } catch (ex) {
+            // If any of the assertions failed then fail the job
+            putJobFailure(ex);
+        }
+    });
+};
+
+
+/*
     //TODO use the dynamohelper once its extracted into its own module
 
     var docClient = new AWS.DynamoDB.DocumentClient();
@@ -40,4 +92,5 @@ exports.handler = (event, context, callback) => {
     });
 
 
-};
+;
+    */
