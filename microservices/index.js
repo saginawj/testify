dda24018476f6c53cc0dd14b7c7a1d88a8125634
Saginaw, Jonathan M <saginawj@gmail.com>
2016-06-10 14:49:04 -0400

    working put with callback

--- microservices/index.js
+++ microservices/index.js
@@ -1,29 +1,57 @@
 console.log('Loading event');
 var AWS = require('aws-sdk');
+AWS.config.update({region: "us-east-1"});
 var dynamodb = new AWS.DynamoDB();
+
+//using doc client
+var DOC = require('dynamodb-doc');
+var docClient = new DOC.DynamoDB();
+
+
+
+
+
 
 exports.handler = function(event, context) {
     console.log("Request received:\n", JSON.stringify(event));
     console.log("Context received:\n", JSON.stringify(context));
     console.log("EVENT:  ", event);
+    console.log("CONTEXT: ", context);
+    console.log("OPERATION: ", event.operation);
 
     var tableName = "sampleTable";
-    var datetime = new Date().getTime().toString();
 
+    var docCallback = function(err, data) {
+        console.log("**RECEIVED DYNAMO CALLBACK**");
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            console.log(data);
+        }
+    }
+
+    var params = {};
+    params.TableName = tableName;
+    params.Item = event.payload.item;
+
+    console.log("**EXECUTE DYNAMO CALL**");
+    docClient.putItem(params, docCallback);
+
+    /*
     dynamodb.putItem({
         "TableName": tableName,
         "Item": {
             "id": {
-                "N": event.id
+                "N": event.payload.item.id
             },
             "date": {
-                "S": event.date
+                "S": event.payload.item.date
             },
             "harness": {
-                "S": event.harness
+                "S": event.payload.item.harness
             },
             "passpercentage": {
-                "N": event.passpercentage
+                "N": event.payload.item.passpercentage
             }
         }
     }, function(err, data) {
@@ -34,4 +62,6 @@ exports.handler = function(event, context) {
             context.succeed('SUCCESS');
         }
     });
+    */
+
 }
