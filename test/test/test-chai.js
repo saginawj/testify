0c2a0423d9edaff14e54518853424ab0a3184eb0
Saginaw, Jonathan M. <saginawj@gmail.com>
2016-06-13 07:18:14 -0400

    foundation for mocha/chai framework

--- test/test/test-chai.js
+++ test/test/test-chai.js
@@ -1,17 +1,35 @@
-var assert = require('chai').assert;
+var assert = require('chai').assert;  //use for TDD, not BDD
+var expect = require('chai').expect;	//use for BDD
+var testCase = require('mocha').describe;
 var expected;
 var current;
 
 before(function(){
-  expected = "helloworld";
+  expectedString = "helloworld";
 })
 
-describe('Strings', function(){
+describe('**TDD Tests with Assert**', function(){
   beforeEach(function(){
    current = 'helloworld';
   })
   
   it('should return a string', function(){
-    assert.equal(current, expected, "strings not the same");
+    assert.equal(current, expectedString, "strings not the same");
   });
 })
+
+describe('**BDD Tests with Expect**', function(){
+  beforeEach(function(){
+   current = 1;
+  })
+  describe('Basics', function(){
+  
+  it('should return a number', function(){
+	  	expect(1).to.equal(1);
+  });
+  it('should equal a string', function(){
+	    expect(expectedString).to.equal("helloworld");
+  });
+  });
+})
+
