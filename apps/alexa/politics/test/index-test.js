a9df366f7306bea7d004e91cb625687840e18d3d
Saginaw, Jonathan M. <saginawj@gmail.com>
2016-06-17 08:01:27 -0400

    added test folder and mocha/chai for Washington Insider

---
+++ apps/alexa/politics/test/index-test.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;	//use for BDD
+
+var history = require('../src/index.js');
+
+
+before(function(){
+  var expectedString = "helloworld";
+})
+
+describe("**Washington Insider Events**", function(){
+
+	describe("Basic Functions", function(){
+
+	       it("return true", function(){
+	    	   expect(true).to.equal(true);
+	           
+	       });
+	   });
+});
