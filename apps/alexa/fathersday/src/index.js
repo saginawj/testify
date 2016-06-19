ac966b481699a2570afd3307cffbbc6845b1a39a
Saginaw, Jonathan M <saginawj@gmail.com>
2016-06-19 10:36:35 -0400

    simple fathers day

--- apps/alexa/fathersday/src/index.js
+++ apps/alexa/fathersday/src/index.js
@@ -5,6 +5,11 @@
 * Defaults to all categories
  */
 
+ var rayaText = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
 var APP_ID = 'amzn1.echo-sdk-ams.app.09d0d0d0-d9c6-4319-b7b1-99440ac40bae';
 
 var https = require('https');
@@ -63,9 +68,8 @@ PoliticsSkill.prototype.intentHandlers = {
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        var speechText = "With Washington Insider, you can get information about policy topics at the White House.  " +
-            "For example, you could say, tell me about the economy, or tell me about education. Now, which category do you want?";
-        var repromptText = "Which category do you want?";
+        var speechText = "I can tell you some factions about yourself.  Just say Raya, or Ethan, or Sammy.";
+        var repromptText = "Who do you want to know more about?";
         var speechOutput = {
             speech: speechText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
@@ -96,8 +100,8 @@ PoliticsSkill.prototype.intentHandlers = {
 
 function getWelcomeResponse(response) {
     var cardTitle = "Father's Day?";
-    var speechText = "Hi Raya, Ethan, and Sammy.  Welcome to our home.  My name is Alexa, and I'm really happy that you are spending Father's Day with me.  I am glad that you came all the way from New Jersey to see me today.  I learned a few facts about you that I'd like to share.  Who would you like to know about?  Just say Raya.  Or Ethan.  Or Sammy.";
-    var repromptText = "<p>Would you like to know about Raya, or Ethan, or Sammy?";
+    var speechText = "Hi Raya, Ethan, and Sammy.  Welcome to our home.  My name is Alexa, and I'm really happy that you are spending Father's Day with me.  Jon and Nancy are really glad that you came all the way from New Jersey.  My only request is that you don't chase Bugsy and Squeaker.  They would really appreciate that.";
+    var repromptText = " ";
     var cardOutput = "Father's Day";
 
     var speechOutput = {
@@ -112,134 +116,17 @@ function getWelcomeResponse(response) {
 }
 
 function handleFirstEventRequest(intent, session, response) {
-    //console.log("**INTENTS** ", intent);
-    var categorySlot = intent.slots.category;
-    var category = "";
-    var repromptText = "With Washington Insider, you can ask the White House about political progress in America.  Just ask: What's up with economy?  The categories are economy, education, and healthcare.";
-    var sessionAttributes = {};
-    sessionAttributes.index = paginationSize;
-
-    console.log("**CATEGORY SLOT** ", categorySlot);
-    if (categorySlot && categorySlot.value) {
-        category = categorySlot.value;
-    } else {
-        category = defaultCategory;
-    }
-
-    console.log("**CATEGORY** ", category);
-
-    var prefixContent = "<p>Here are the latest policies for " + category + "</p>";
-
-    var cardContent = "New policies";
-
-    var cardTitle = "Categories in " + category;
-
-    getJsonEventsFromWhiteHouse(category, function (events) {
-        var speechText = "",
-            i;
-        sessionAttributes = events;
-        session.attributes = sessionAttributes;
-        if (events.length == 0) {
-            speechText = "There is a problem connecting to White House website at this time. Please try again later.";
-            cardContent = speechText;
-            response.tell(speechText);
-        } else {
-            for (i = 0; i < paginationSize; i++) {
-                cardContent = cardContent + events[i] + " ";
-                speechText = "<p>" + speechText + events[i] + "</p> ";
-            }
-            speechText = speechText + " <p>Want another policy topic?</p>";
-            var speechOutput = {
-                speech: "<speak>" + prefixContent + speechText + "</speak>",
-                type: AlexaSkill.speechOutputType.SSML
-            };
-            var repromptOutput = {
-                speech: repromptText,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
-        }
-    });
+    
 }
 
 /**
  * Gets a poster prepares the speech to reply to the user.
  */
 function handleNextEventRequest(intent, session, response) {
-    var cardTitle = "More Policies",
-        sessionAttributes = session.attributes,
-        result = sessionAttributes;
-        speechText = "",
-        cardContent = "",
-        repromptText = "Do you want to know more about what happened in this category?",
-        i;
-    console.log("**RESULT**: ", result);
-
-    if (!result) {
-        speechText = "With Washington Insider, you can get the latest political news.   What category do you want?";
-        cardContent = speechText;
-    } else if (sessionAttributes.index >= result.length) {
-        speechText = "There are no more policies for this category. Try another category by saying <break time = \"0.3s\"/> get policies for education.";
-        cardContent = "There are no more policies for this category.  Try another category by saying: get policies for education.";
-    } else {
-        for (i = 0; i < paginationSize; i++) {
-            if (sessionAttributes.index>= result.length) {
-                break;
-            }
-            speechText = speechText + "<p>" + result[sessionAttributes.index] + "</p> ";
-            speechText = speechText + " <p>Want another policy topic?</p>";
-            cardContent = cardContent + result[sessionAttributes.index] + " ";
-            sessionAttributes.index++;
-        }
-        if (sessionAttributes.index < result.length) {
-            speechText = speechText + " Want more policy information?";
-            cardContent = cardContent + " Want more policy information?";
-        }
-    }
-    var speechOutput = {
-        speech: "<speak>" + speechText + "</speak>",
-        type: AlexaSkill.speechOutputType.SSML
-    };
-    var repromptOutput = {
-        speech: repromptText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
-    };
-    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
+   
 }
 
-function getJsonEventsFromWhiteHouse(category, eventCallback) {
-    var url = urlPrefix + "all/" + category;
-    console.log("**URL** " + url);
 
-    https.get(url, function(res) {
-        var body = '';
-
-        res.on('data', function (chunk) {
-            body += chunk;
-        });
-
-        res.on('end', function () {
-            var stringResult = parseJson(body);
-            eventCallback(stringResult);
-        });
-    }).on('error', function (e) {
-        console.log("Got error: ", e);
-    });
-}
-
-function parseJson(inputText){
-
-    var issues = {};
-    issues.index = paginationSize;
-    var json = JSON.parse(inputText);
-    var count = json.length;
-    console.log("**EVENT COUNT** ", count);
-
-    for(i=0; i<count; i++) {
-        issues[i] = json[i].body;
-    }
-    return issues;
-}
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
