d177bdf94bfcbb9ebf2f95716ec61c6442e2a185
Saginaw, Jonathan M <saginawj@gmail.com>
2016-06-29 19:12:15 -0400

    added mocha

--- apps/parser/test/parser-test.js
+++ apps/parser/test/parser-test.js
@@ -1,9 +1,23 @@
 var mocha = require('mocha');
-var chai = require('chai');
+var expect = require('chai').expect;
 
 var parser = require('../src/parser.js');
 
+describe('**Parser Class**', function(){
+
+  it('should return a default test', function(){
+	var current = 'helloworld';
+    expect(current).to.equal('helloworld');
+  });
+
+  it('should return a file', function(){
+	var json = parser.jsonParse('../data/event.json');
+	expect(json).is.not.null;
+  });
 
 
 
-parser.jsonParse();
+})
+
+
+
